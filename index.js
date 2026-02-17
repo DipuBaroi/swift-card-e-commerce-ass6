8038e3d62187c209d936dc9492302ecf1d207201
DipuBaroi <dipubaroi758@gmail.com>
2026-02-17 16:27:38 +0600

    display all card

--- index.js
+++ index.js
@@ -17,4 +17,45 @@ const displayCategory = (categories) =>{
         categoryContainer.append(categoryBtn)
     }
 }
+
+const loadProduct = ()=>{
+    const url ='https://fakestoreapi.com/products'
+    fetch(url)
+    .then(res =>res.json())
+    .then(data=>dispayProduct(data))
+}
+
+const dispayProduct =(products)=>{
+    const productContainer = document.getElementById('product-container')
+    productContainer.innerHTML="";
+    products.forEach(product=>{
+        console.log(product);
+        const productDiv = document.createElement('div')
+        productDiv.innerHTML=`
+        
+        <div class="p-4 space-y-5 shadow-lg rounded-lg lg:h-full ">
+                <div class="bg-gray-200 " ><img  src=${product.image} alt=""></div>
+                <div class="flex justify-between items-center ">
+                    <h3 class=" font-medium text-blue-700 bg-blue-200 rounded-xl px-3">${product.category}</h3>
+                    <div class="flex items-center">
+                        <i class="fa-solid fa-star text-yellow-500"></i>
+                        <h2>${product.rating.rate}</h2>
+                        <h2>(${product.rating.count})</h2>
+
+                    </div>
+                </div>
+                <p class="line-clamp-1">${product.description}</p>
+                <h2 class="text-xl font-bold">$${product.price}</h2>
+                <div class="flex justify-between items-center">
+                    <button class="btn btn-active px-5"><i class="fa-regular fa-eye"></i> Details </button>
+                    <button class="btn btn-active btn-primary px-6"><i class="fa-solid fa-cart-shopping"></i> Add</button>
+                </div>
+            </div>
+
+        
+        `
+        productContainer.append(productDiv)
+    })
+}
+loadProduct()
 loadCategory()
